9585d43f78be56eb55abf858fc02ea12fd4c8369
RomanOdessa75 <115027650+RomanOdessa75@users.noreply.github.com>
2025-05-22 18:07:18 +0300

    fix: button & input types

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -1,6 +1,8 @@
-interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {}
-
-const Button: React.FC<ButtonProps> = ({ className, children, ...props }) => {
+const Button: React.FC<React.ButtonHTMLAttributes<HTMLButtonElement>> = ({
+  className = "",
+  children,
+  ...props
+}) => {
   return (
     <button
       className={`px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 transition ${className}`}
--- src/components/ui/Input.tsx
+++ src/components/ui/Input.tsx
@@ -1,18 +1,17 @@
 import { forwardRef } from "react";
 
-interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {}
-
-const Input = forwardRef<HTMLInputElement, InputProps>(
-  ({ className, ...props }, ref) => {
-    return (
-      <input
-        ref={ref}
-        className={`border px-3 py-2 rounded focus:outline-none focus:ring-2 focus:ring-blue-500 ${className}`}
-        {...props}
-      />
-    );
-  }
-);
+const Input = forwardRef<
+  HTMLInputElement,
+  React.InputHTMLAttributes<HTMLInputElement>
+>(({ className = "", ...props }, ref) => {
+  return (
+    <input
+      ref={ref}
+      className={`border px-3 py-2 rounded focus:outline-none focus:ring-2 focus:ring-blue-500 ${className}`}
+      {...props}
+    />
+  );
+});
 
 Input.displayName = "Input";
 export { Input };
